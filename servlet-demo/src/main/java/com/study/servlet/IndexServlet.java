1eab3678c7409a73bc434993fda8d23248ebefbb
tianyaning <tianyaning@meituan.com>
2019-07-05 00:18:54 +0800

    demo

--- servlet-demo/src/main/java/com/study/servlet/IndexServlet.java
+++ servlet-demo/src/main/java/com/study/servlet/IndexServlet.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String response = "";
-        response += "hello world\n";
+        response += "HTTP/1.1 200 OK\r\n";
+        response += "Content-Length: 16\r\n\r\n";
+        response += "Hello worldvvvvv";
         httpServletResponse.getOutputStream().write(response.getBytes());
         httpServletResponse.getOutputStream().flush();
     }
--- servlet-demo/src/main/java/com/study/servlet/PostServlet.java
+++ servlet-demo/src/main/java/com/study/servlet/PostServlet.java
@@ -1,4 +1,22 @@
 package com.study.servlet;
 
-public class PostServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String name = new String(req.getParameter("name"));
+        String age = new String(req.getParameter("age"));
+        String response = "";
+        response += "HTTP/1.1 200 OK\r\n";
+        response += "name = "+ name +"\n";
+        response += "age = "+ age;
+        resp.getOutputStream().write(response.getBytes());
+        resp.getOutputStream().flush();
+    }
 }
